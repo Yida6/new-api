ad14662182edbc858cb4a82377145d67b32d5b94
yida <yida@gmail>
2026-08-20 11:31:58 +0800

    feat(controller): restrict ordinary users to their own group in group options

--- controller/group.go
+++ controller/group.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 
+	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/model"
 	"github.com/QuantumNous/new-api/service"
 	"github.com/QuantumNous/new-api/setting"
@@ -29,6 +30,20 @@ func GetUserGroups(c *gin.Context) {
 	userId := c.GetInt("id")
 	userGroup, _ = model.GetUserGroup(userId, false)
 	userUsableGroups := service.GetUserUsableGroups(userGroup)
+
+	// 普通用户只能选择自己所属的分组；管理员（root/admin）保留全部可用组。
+	// Token/Playground 的分组选项均来自本接口：收窄后普通用户无法把 Token
+	// 建到别人的分组，也就无法使用他人分组下的渠道。
+	if userId > 0 && userGroup != "" {
+		if user, err := model.GetUserCache(userId); err == nil && user.Role < common.RoleAdminUser {
+			ownDesc, ok := userUsableGroups[userGroup]
+			userUsableGroups = map[string]string{}
+			if ok {
+				userUsableGroups[userGroup] = ownDesc
+			}
+		}
+	}
+
 	for groupName, _ := range ratio_setting.GetGroupRatioCopy() {
 		// UserUsableGroups contains the groups that the user can use
 		if desc, ok := userUsableGroups[groupName]; ok {
